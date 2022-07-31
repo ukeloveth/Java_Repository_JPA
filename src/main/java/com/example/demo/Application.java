42dac6c178bda381a5df22b8e60ed5b877f6791e
ukeloveth <ukeloveth247@gmail.com>
2022-07-31 17:02:12 +0100

    My fisrt commit

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+    public static void main(String[] args) {SpringApplication.run(Application.class, args);
+    }
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student maria= new Student(
+                    1L,
+                    "Maria",
+                    "Jones",
+                    "maria123@gmail.com",
+                    21
+            );
+            studentRepository.save(maria);
+        };
+
     }
 
 }
